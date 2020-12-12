94ff9fe9a08725167a449c41c770866368f9a395
kobayashi-kenta <12321klj@gmail.com>
2020-12-12 16:43:24 +0900

    Initial Commit

---
+++ CardApp/CardAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardAppApp.swift
+//  CardApp
+//
+//  Created by Kenta on 2020/12/12.
+//
+
+import SwiftUI
+
+@main
+struct CardAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
